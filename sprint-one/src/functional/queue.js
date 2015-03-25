ab2f85dadb6302716362556b6e30056f2a73ee99
Lindsay Pond <lepond@gmail.com>
2015-03-25 15:52:02 -0700

    Completed Queue and Stack for Functional Case.

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,18 +1,28 @@
 var Queue = function(){
   var someInstance = {};
-
+  var front = 0;
+  var end = 0;
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+    end++;
+    storage[end] = value;
   };
 
   someInstance.dequeue = function(){
+    if (end > front) {
+      front++;
+      var temp = storage[front];
+      delete storage[front];
+      return temp;
+    }
   };
 
   someInstance.size = function(){
+    return end - front;
   };
 
   return someInstance;
