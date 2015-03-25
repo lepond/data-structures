be43b9f4705e1ac3674fa300039d95ba0a2a8d1b
Lindsay Pond <lepond@gmail.com>
2015-03-25 12:02:47 -0700

    Completed Functional Stack for Data Structures.

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,4 +1,5 @@
 var Stack = function(){
+  var stackSize = 0;
   var someInstance = {};
 
   // Use an object with numeric keys to store values
@@ -6,13 +7,23 @@ var Stack = function(){
 
   // Implement the methods below
   someInstance.push = function(value){
+    stackSize++;
+    storage[stackSize] = value;
   };
 
   someInstance.pop = function(){
+    if (stackSize > 0) {
+      var temp = storage[stackSize];
+      delete storage[stackSize];
+      stackSize--;
+      return temp;
+    } 
   };
 
   someInstance.size = function(){
+    return stackSize;
   };
 
   return someInstance;
 };
+
