939420fcfa2212a26011daf3e3b70d1ddee7ba33
Lindsay Pond <lepond@gmail.com>
2015-03-25 16:29:16 -0700

    Completed Shared Function case for Queues.

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,9 +1,31 @@
 var Queue = function(){
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = {};
+  obj.first = 0;
+  obj.last = 0;
+  obj.storage = {};
+  _.extend(obj, queueMethods);
+  return obj;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  size: function (){
+    return this.last - this.first;
+  },
+  enqueue: function (value){
+    this.last++;
+    this.storage[this.last] = value;
+  },
+  dequeue: function() {
+    if (this.last > this.first) {
+      this.first++;
+      var temp = this.storage[this.first];
+      delete this.storage[this.first];
+      return temp;
+    }
+  }
+};
 
 
 
