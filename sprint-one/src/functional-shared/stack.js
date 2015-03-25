329df0f55454bc9cc5ed487a9be7199b3b0e3146
Lindsay Pond <lepond@gmail.com>
2015-03-25 16:16:39 -0700

    Completed Shared Functions Case for Stack.

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,27 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = {};
+  obj.storage = {};
+  obj.stackSize = 0;
+  _.extend(obj, stackMethods);
+  return obj;
 };
 
-var stackMethods = {};
-
-
+var stackMethods = {
+  size: function () {
+    return this.stackSize;
+  },
+  push: function (value) {
+    this.stackSize++;
+    this.storage[this.stackSize] = value;
+  },
+  pop: function () {
+    if (this.stackSize > 0) {
+      var temp = this.storage[this.stackSize];
+      delete this.storage[this.stackSize];
+      this.stackSize--;
+      return temp;
+    }
+  }
+};
