b041ed2f5d6eab9c6cfdbd14b183055cf0b11b10
Lindsay Pond <lepond@gmail.com>
2015-03-26 10:56:42 -0700

    Completed pseudoclassical case for Stack.

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,27 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = Object.create(queueMethods);
+  obj.storage = {};
+  obj.first = 0;
+  obj.last = 0;
+  return obj;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  size: function () { return this.last - this.first;},
+  enqueue: function (value) { 
+    this.last++; 
+    this.storage[this.last] = value;
+  },
+  dequeue: function () { 
+    if (this.last > this.first) {
+      this.first++;
+      var temp = this.storage[this.first];
+      delete this.storage[this.first];
+      return temp;
+    }
+  }
+};
 
 
