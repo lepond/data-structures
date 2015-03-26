16ac6221603467763d6729a09890af496638deec
Lindsay Pond <lepond@gmail.com>
2015-03-25 17:00:27 -0700

    Completed Prototypal Stack.

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,27 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  // create vare using Object.create
+  // set recurring properties
+  var obj = Object.create(stackMethods);
+  obj.stackSize = 0;
+  obj.storage = {};
+  return obj;
 };
 
-var stackMethods = {};
+var stackMethods = { 
+  size: function(){ return this.stackSize;} ,  
+  push: function (value) { 
+    this.stackSize++; 
+    this.storage[this.stackSize] = value;},
+  pop: function () {
+    if (this.stackSize > 0) {
+      var temp = this.storage[this.stackSize];
+      delete this.storage[this.stackSize];
+      this.stackSize--; 
+      return temp;
+    }
+ }
+};
 
 
