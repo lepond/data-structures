f80a9918d17717dab5e9d399f66388a2bcd8b3c2
Lindsay Pond <lepond@gmail.com>
2015-03-27 11:15:03 -0700

    Completed Graph for Sprint 2 and Updated test errors for Graph.

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,31 +1,55 @@
 
 
 var Graph = function(){
+  this.nodes = {};
 };
 
 Graph.prototype.addNode = function(node){
+  this.nodes[node] = {};
 };
 
 Graph.prototype.contains = function(node){
+  if (this.nodes[node] !== undefined) {
+    return true;
+  }
+  return false;
 };
 
 Graph.prototype.removeNode = function(node){
+  delete this.nodes[node];
 };
 
 Graph.prototype.hasEdge = function(fromNode, toNode){
+  if (this.nodes[fromNode][toNode] !== undefined) {
+    return true;
+  }
+  return false;
 };
 
 Graph.prototype.addEdge = function(fromNode, toNode){
+  this.nodes[fromNode][toNode] = true;
+  this.nodes[toNode][fromNode] = true;
 };
 
 Graph.prototype.removeEdge = function(fromNode, toNode){
+  delete this.nodes[fromNode][toNode];
+  delete this.nodes[toNode][fromNode];
 };
 
 Graph.prototype.forEachNode = function(cb){
+  for (var key in this.nodes) {
+    cb(key);
+  }
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+  contains = O(1);
+  removeNode = O(1);
+  hasEdge = O(1);
+  addEdge = O(1);
+  forEachNode = O(n to n^2);
+  addNode = 0(1);
  */
 
 
